530ecabb5d6593ee824483a50ce74302ef7588e7
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2024-11-11 22:42:44 +0330

    "comfy store - add login form input, add login page, add login page submit btn, add index for component"

---
+++ 15-comfy-store/src/Components/SubmitBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubmitBtn = ({ text }) => {
+  return <button className="btn btn-primary ">{text}</button>;
+};
+
+export default SubmitBtn;
--- 15-comfy-store/src/Pages/Login.jsx
+++ 15-comfy-store/src/Pages/Login.jsx
@@ -1,5 +1,34 @@
+import { FormInput, SubmitBtn } from "../Components";
+import { Form, Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="post"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="email"
+          label="email"
+          name="identifier"
+          DefaultValue="test@test.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          DefaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-neutral btn-outline">
+          <Link to="/Register">Register</Link>
+        </button>
+      </Form>
+    </section>
+  );
 };
 
 export default Login;
