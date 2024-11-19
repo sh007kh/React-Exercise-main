de69f35d3a0bb94ae7581dcc4b988f066bf000c7
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2024-11-19 18:57:04 +0330

    "comfy store - add theme in tailwind config, add Navlinks component,add theme in navbar"

--- 15-comfy-store/src/Components/Navbar.jsx
+++ 15-comfy-store/src/Components/Navbar.jsx
@@ -1,8 +1,27 @@
 import { NavLink } from "react-router-dom";
 import { FaBarsStaggered } from "react-icons/fa6";
-import { BsCart3 } from "react-icons/bs";
+import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+import NavLinks from "./NavLinks";
+import { useEffect, useState } from "react";
 
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem("theme") || "winter";
+};
 const Navbar = () => {
+  const themes = {
+    winter: "winter",
+    dracula: "dracula",
+  };
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+  const handleTheme = () => {
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+    setTheme(newTheme);
+  };
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -19,16 +38,23 @@ const Navbar = () => {
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <FaBarsStaggered className="h-6 w-6" />
             </label>
-            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
-              nav links
+            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">nav links</ul>
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* theme */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            <BsSunFill className="swap-on h-4 w-4" />
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
           {/* cart link */}
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4'">
             <div className="indicator">
