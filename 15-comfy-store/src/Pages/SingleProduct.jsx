c802418facbc4cfb31decaebe04b3f91b1e68726
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2024-12-09 17:24:29 +0330

    "comfy store - edit index.js to index.jsx in utils , edit singleProduct.jsx"

--- 15-comfy-store/src/Pages/SingleProduct.jsx
+++ 15-comfy-store/src/Pages/SingleProduct.jsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router-dom";
-import { customFetch, formatPrice } from "../Utils";
+import { Link, useLoaderData } from "react-router-dom";
+import { customFetch, formatPrice, generateAmountOptions } from "../Utils";
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
@@ -8,11 +9,91 @@ export const loader = async ({ params }) => {
 };
 const SingleProduct = () => {
   const { product } = useLoaderData();
-  console.log(product);
   const { image, title, price, company, description, colors } =
     product.attributes;
   const dollarAmount = formatPrice(price);
-  return <div>SingleProduct</div>;
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+  const handleAmount = (e) => {
+    setAmount(parseInt(e.target.value));
+  };
+  return (
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+      {/* PRODUCT */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className="h-96 w-96 rounded-lg lg:w-full object-cover"
+        />
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl text-neutral-content font-bold mt-2">
+            {company}
+          </h4>
+          <p className="mt-3 text-xl">{dollarAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+        </div>
+        {/* colors */}
+        <div className="mt-6 ">
+          <h4 className="text-md font-medium tracking-wider capitalize">
+            colors
+          </h4>
+          <div className="mt-2 ">
+            {colors.map((color) => {
+              return (
+                <button
+                  type="button"
+                  key={color}
+                  className={`badge w-6 h-6 mr-2 ${
+                    color === productColor && "border-2 border-secondary"
+                  }`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => setProductColor(color)}
+                ></button>
+              );
+            })}
+          </div>
+          {/* ADD TO CART */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label" htmlFor="amount">
+              <h4 className="text-md font-medium tracking-wider capitalize">
+                amount
+              </h4>
+            </label>
+            <select
+              className="select select-secondary select-bordered select-md"
+              id="amount"
+              value={amount}
+              onChange={handleAmount}
+            >
+              {generateAmountOptions(10)}
+            </select>
+          </div>
+          <div className="mt-10">
+            <button
+              className="btn btn-secondary btn-md"
+              onClick={() => console.log("add to bag")}
+            >
+              add to bag
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
