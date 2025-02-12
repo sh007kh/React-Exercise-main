152183b91eb0d5cbd45cfbaffee868f8c3e42818
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2025-02-12 12:32:20 +0330

    "comfy store - add additem remove item clearcart function , refractor code in cartslice.js"

--- 15-comfy-store/src/Features/CartSlice.js
+++ 15-comfy-store/src/Features/CartSlice.js
@@ -9,10 +9,12 @@ const defaultState = {
   tax: 0,
   orderTotal: 0,
 };
-
+const getFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("cart")) || defaultState;
+};
 const cartSlice = createSlice({
   name: "cart",
-  initialState: defaultState,
+  initialState: getFromLocalStorage(),
   reducers: {
     addItem: (state, action) => {
       console.log(action.payload);
@@ -26,16 +28,36 @@ const cartSlice = createSlice({
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
 
-      state.tax = 0.1 * state.cartTotal;
-
-      state.orderTotal = state.cartTotal + state.shipping + state.tax;
-
       toast.success("Item added to cart");
+      cartSlice.caseReducers.calculateTotals(state);
+    },
+    clearCart: (state) => {
+      localStorage.setItem("cart", JSON.stringify(defaultState));
+      return defaultState;
+    },
+    removeItem: (state, action) => {
+      const { cartID } = action.payload;
+      const product = state.cartItems.find((i) => i.cartID === cartID);
+      state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID);
+      state.numItemsInCart -= product.amount;
+      state.cartTotal -= product.price * product.amount;
+      toast.error("Item removed from cart");
+      cartSlice.caseReducers.calculateTotals(state);
+    },
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload;
+      const item = state.cartItems.find((i) => i.cartID === cartID);
+      state.numItemsInCart += amount - item.amount;
+      state.cartTotal += item.price * (amount - item.amount);
+      item.amount = amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.success("Cart updated");
+    },
+    calculateTotals: (state) => {
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
       localStorage.setItem("cart", JSON.stringify(state));
     },
-    clearCart: (state) => {},
-    removeItem: (state, action) => {},
-    editItem: (state, action) => {},
   },
 });
 
